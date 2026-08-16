57dfcaae1fdd444c9722f1384756d926331b7ca0
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-08-16 12:05:47 +0500

    Created getAllProducts api which will retrieve products data from database

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -33,11 +33,40 @@ export const createProduct = async (req, res) => {
     return res.status(200).json({
       success: true,
       message: "Product Created Successfully",
-      product
+      product,
     });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+//get products from db
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const allProducts = await Product.find();
+
+    //checks
+    if (!allProducts) {
+      res.status(401).json({
+        success: false,
+        message: "Products not found",
+      });
+    }
+
+    //products found
+    return res.status(200).json({
+      success: true,
+      allProducts,
+      message: "Products are successfully found",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
       success: false,
       error,
     });
--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createProduct } from "../controllers/productsController.js";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controllers/productsController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/create-product", createProduct);
+productRouter.get("/get-all-products", getAllProducts);
 
 export default productRouter;
